c573c924935bfeddb8ede985ac1be0a9872c7dc2
RajuKoushik <g.rajukoushik@gmail.com>
2016-09-11 02:02:36 +0530

    Updated addPost API

--- api/views.py
+++ api/views.py
@@ -8,8 +8,45 @@ from django.views.decorators.csrf import csrf_exempt
 from . import functions
 from .models import *
 
+"""
+Login a user
+"""
 
 
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        user_name = request.POST.get('user_name')
+        password = request.POST.get('password')
+        user = User.objects.filter(user_name=user_name, password=password)
+        if len(user) == 1:
+            resp = functions.Response()
+            cuser = user[0]
+
+            resp.add('firstname', cuser.first_name)
+            resp.add('lastname', cuser.last_name)
+            resp.add('email', cuser.email)
+            resp.add('user_name', cuser.user_name)
+            posts = Post.objects.all()
+            postitles = []
+            posttexts = []
+            postvotes = []
+
+            counter = 0
+            for i in posts:
+                postitles[counter] = i.post_name
+                posttexts[counter] = i.post_text
+                postvotes[counter] = i.post_votes
+                counter = counter + 1
+            resp.add('posttitles', postitles)
+            resp.add('postexts', posttexts)
+            resp.add('postvotes', postvotes)
+            return resp.respond()
+
+        else:
+            return functions.auth_failed()
+    else:
+        return functions.invalid_option()
 
 
 """
@@ -40,42 +77,42 @@ def register(request):
     else:
         return functions.invalid_option()
 
-    """
-    Login a user
-    """
 
-    @csrf_exempt
-    def login(request):
-        if request.method == 'POST':
-            user_name = request.POST.get('user_name')
-            password = request.POST.get('password')
-            user = User.objects.filter(user_name=user_name, password=password)
-            if len(user) == 1:
-                resp = functions.Response()
-                cuser = user[0]
+def addpost(request):
+    if request.method == 'POST':
+        post_name = request.POST.get('post_name')
+        post_text = request.POST.get('post_text')
+        post_tag = request.POST.get('post_tag')
+        post_catid = request.POST.get('post_catid')
+        user_name = request.POST.get('user_name')
+        try:
+            post = Post(post_name=post_name,post_text=post_text,post_tag=post_tag,post_catid=post_catid,user_id = User.objects.filter(user_name=user_name))
+            post.save()
 
 
-                resp.add('firstname', cuser.first_name)
-                resp.add('lastname', cuser.last_name)
-                resp.add('email', cuser.email)
-                resp.add('user_name', cuser.user_name)
-                posts = Post.objects.filter(user_id=cuser)
-                postitles = []
-                posttexts = []
-                postvotes = []
 
-                counter = 0
-                for i in posts:
-                    postitles[counter] = i.post_name
-                    posttexts[counter] = i.post_text
-                    postvotes[counter] = i.post_votes
-                    counter = counter + 1
-                resp.add('posttitles',postitles)
-                resp.add('postexts',posttexts)
-                resp.add('postvotes',postvotes)
-                return resp.respond()
+            resp = functions.Response()
 
-            else:
-                return functions.auth_failed()
+            posts = Post.objects.all()
+            postitles = []
+            posttexts = []
+            postvotes = []
+
+            counter = 0
+            for i in posts:
+                postitles[counter] = i.post_name
+                posttexts[counter] = i.post_text
+                postvotes[counter] = i.post_votes
+                counter = counter + 1
+            resp.add('posttitles', postitles)
+            resp.add('postexts', posttexts)
+            resp.add('postvotes', postvotes)
+
+            return resp.respond()
+        except Exception as e:
+            print(e)
+            return functions.user_exists()
         else:
             return functions.invalid_option()
+
+
