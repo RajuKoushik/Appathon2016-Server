1994467149866a853f48689a009b0e8f05a05e41
RajuKoushik <g.rajukoushik@gmail.com>
2016-09-10 14:39:19 +0530

    Updated models.py .

--- api/models.py
+++ api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.PositiveIntegerField(default=0)
+    email = models.EmailField()
+    password = models.CharField(max_length=200)
+    join_date = models.DateTimeField('Join Date')
+
+
+    def __str__(self):
+        return self.nick_name
+        # adding a custom database
+
+class Post(models.Model):
+    user_id = models.ForeignKey(User)
+    post_name = models.CharField(max_length=30)
+    post_text = models.CharField(max_length=200)
+    post_tag = models.IntegerField()
+    post_catid = models.IntegerField(default=0)
+    post_votes = models.IntegerField()
+
+    def __str__(self):
+        return self.post_name
+
